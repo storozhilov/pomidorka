85cce2972c6eee4815090c17db169ddc185c89e1
illy <illy@9a9a0cbe-8417-48fa-846c-a9c3a6d0168c>
2013-07-31 10:22:18 +0000

    MODBUS endpoint device test has been amended: write register test has been added and debugging output is turned on.

--- tests/endpoint_device_test.cxx
+++ tests/endpoint_device_test.cxx
@@ -12,6 +12,54 @@
 
 #define REGISTERS_ADDRESS 0
 #define REGISTERS_AMOUNT 5
+#define WRITE_REGISTER_ADDRESS 1
+#define WRITE_REGISTER_FIRST_VALUE 127
+#define WRITE_REGISTER_SECOND_VALUE 255
+
+void test_read_registers(modbus_t * ctx)
+{
+	// Reading registers
+	uint16_t registers[REGISTERS_AMOUNT];
+	printf("Reading registers ... ");
+	int registersFetched = modbus_read_registers(ctx, REGISTERS_ADDRESS, REGISTERS_AMOUNT, registers);
+	if (registersFetched < 0) {
+		fprintf(stderr, "Reading registers error: '%s'\n", modbus_strerror(errno));
+		return;
+	} else {
+		printf("OK\n");
+	}
+	sleep(1);
+	// Reading registers again
+	printf("Reading registers again ... ");
+	registersFetched = modbus_read_registers(ctx, REGISTERS_ADDRESS, REGISTERS_AMOUNT, registers);
+	if (registersFetched < 0) {
+		fprintf(stderr, "Reading registers error: '%s'\n", modbus_strerror(errno));
+		return;
+	} else {
+		printf("OK\n");
+	}
+}
+
+void test_write_register(modbus_t * ctx)
+{
+	// Writing register
+	printf("Writing register ... ");
+	if (modbus_write_register(ctx, WRITE_REGISTER_ADDRESS, WRITE_REGISTER_FIRST_VALUE) < 0) {
+		fprintf(stderr, "Writing register error: '%s'\n", modbus_strerror(errno));
+		return;
+	} else {
+		printf("OK\n");
+	}
+	sleep(1);
+	// Writing register again
+	printf("Writing register again ... ");
+	if (modbus_write_register(ctx, WRITE_REGISTER_ADDRESS, WRITE_REGISTER_FIRST_VALUE) < 0) {
+		fprintf(stderr, "Writing register error: '%s'\n", modbus_strerror(errno));
+		return;
+	} else {
+		printf("OK\n");
+	}
+}
 
 int main(int argc, char *argv[])
 {
@@ -21,6 +69,7 @@ int main(int argc, char *argv[])
 		fprintf(stderr, "MODBUS RTU context creation error: '%s'\n", modbus_strerror(errno));
 		return 1;
 	}
+	modbus_set_debug(ctx, 1);
 	if (modbus_set_slave(ctx, DEVICE_ID) < 0) {
 		fprintf(stderr, "MODBUS slave setting error: '%s'\n", modbus_strerror(errno));
 		modbus_free(ctx);
@@ -31,27 +80,9 @@ int main(int argc, char *argv[])
 		modbus_free(ctx);
 		return 1;
 	}
-	// Reading registers
-	uint16_t registers[REGISTERS_AMOUNT];
-	printf("Reading registers ... ");
-	int registersFetched = modbus_read_registers(ctx, REGISTERS_ADDRESS, REGISTERS_AMOUNT, registers);
-	if (registersFetched < 0) {
-		fprintf(stderr, "Reading registers error: '%s'\n", modbus_strerror(errno));
-		modbus_free(ctx);
-		return 1;
-	} else {
-		printf("OK\n");
-	}
+	test_read_registers(ctx);
 	sleep(1);
-	printf("Reading registers again ... ");
-	registersFetched = modbus_read_registers(ctx, REGISTERS_ADDRESS, REGISTERS_AMOUNT, registers);
-	if (registersFetched < 0) {
-		fprintf(stderr, "Reading registers error: '%s'\n", modbus_strerror(errno));
-		modbus_free(ctx);
-		return 1;
-	} else {
-		printf("OK\n");
-	}
+	test_write_register(ctx);
 	modbus_free(ctx);
 	return 0;
 }
